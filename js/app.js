1f34200594abf25b6a1b045428179037c4ff345d
Md Nayeem Hossain <nayeemhossain108@gmail.com>
2021-09-17 20:54:34 +0600

    style added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ const loadCountries=()=>{
     .then(res=>res.json())
     .then(data=>displayCountries(data))
 }
-const displayCountries=country=>{
-    console.log(country)
+const displayCountries=countries=>{
+    // console.log(countries)
+    const container=document.getElementById("countries")
+    const countryHTML=countries.map(country=>getCountryHTML(country))
+    container.innerHTML=countryHTML.join(' ')
 }
+const getCountryHTML=country=>{
+    return `
+    <div>
+    <h2>${country.name} </h2>
+    <h4>${country.capital}</h4>
+    <img src="${country.flag}"
+    </div>   `
+}
+loadCountries()
